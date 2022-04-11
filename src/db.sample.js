bca5766c5bb50b9bb31390b874d5a63809bf82ff
Kushan Shamika <shamikakushan@gmail.com>
2022-04-11 22:46:25 +0530

    🧑‍💻 Add sample DB config

---
+++ src/db.sample.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp.firestore();
